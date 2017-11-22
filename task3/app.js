fe752e5a82b627e5178b445d0e34666fa63c5cc0
pelpro <pelpro@yandex.ru>
2017-11-23 00:05:49 +0300

    task3

--- task3/app.js
+++ task3/app.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){
-$("#root").append("<ul type = "circle" id = "list"> </ul>")
-$("#list").append("<li class = "element"><span>Сделать задание №3 по web-программированию</span></li>")
-$(".element").append("<button class = "delete">Удалить</button>")
-$("body").on("click", ".delete", function(){
-$(this).parent().remove()
-})
-$("#root").append("<input id = "add_task_input"></input>").append("<button id = "add_task">Добавить</button>")
-$("#add_task").click(function(){
-$("#tasklist").append("<li class = "element"><span>" + $("#add_task_input").val() + "</span><button class = "delete">Удалить</button></li>")
-}) 
-$("body").keyup(function(eventObject){
-if (eventObject.which === 1) {
-$("#tasklist").append("<li class = "element"><span>" + $("#add_task_input").val() + "</span><button class = "delete">Удалить</button></li>")
+function CreateandAdd(s) {
+var element = $('<li><span></span> <button class="deleteb">Удалить</button></li>');
+$('span', element).text(s);
+$('#root ul').append(element);
+$('.deleteb', element).click(function(delq){
+$(this).parent().remove()});
 }
-})
-})
+$(function(){
+$('#root').append('<ul></ul>');
+$('#root').append('<input id="add_task_input">')
+$('#root').append('<button id="add_task">Добавить</button>');
+$('#add_task').click(function(){
+CreateandAdd($('#add_task_input').val())
+}
+);
+CreateandAdd("Сделать задание #3 по web-программированию");
+});
+
