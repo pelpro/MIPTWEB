e2af22bf34f1cf84f34d44974f73757262671a4c
pelpro <pelpro@yandex.ru>
2017-11-29 21:01:16 +0300

    task4

---
+++ task4/solve.go
@@ -0,0 +1,38 @@
+package main
+import "unicode"
+func RemoveEven(mass []int) []int {
+odd := make([]int, 0)
+for _, number := range mass {
+if number % 2 == 1 {
+mass = append(odd, number)
+}
+}
+return odd
+}
+
+func PowerGenerator(a int) func() int {
+powern := 1
+return func() int {
+powern = powern*a
+return powern
+}
+}
+
+func DifferentWordsCount(str string) int {
+maps := make(map[string]bool)
+countw := 0
+word := ""
+for _, sym := range (x + " ") {
+if unicode.IsLetter(sym) {
+word = word + string(unicode.ToLower(sym))
+}
+else if word != "" {
+if !maps[word] {
+countw += 1
+}
+maps[word] = true
+word = ""
+}
+}
+return countw
+}
